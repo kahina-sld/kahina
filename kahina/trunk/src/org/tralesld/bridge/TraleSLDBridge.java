7e22e7bd0a5ec8aab3ddf5e34dadacad5f88c8aa
ke <ke@6e8e43ff-ab70-0410-9a54-f4433d99c037>
2010-03-22 15:51:58 +0000

    Added further revised methods to Jasper interface.

--- kahina/trunk/src/org/tralesld/bridge/TraleSLDBridge.java
+++ kahina/trunk/src/org/tralesld/bridge/TraleSLDBridge.java
@@ -15,71 +15,106 @@ import org.kahina.io.database.DatabaseHandler;
 
 public class TraleSLDBridge extends LogicProgrammingBridge
 {
-    public void start()
-    {
-        System.err.print("Trying to build GUI window... ");
-        try
-        {
-        	// TODO Softcode choice between DbDataManager and MemDataManager.
-            kahina = new KahinaInstance(new DbDataManager(new DatabaseHandler()));
-            control = new KahinaController();
-            gui = new KahinaGUI(kahina, control);
-            System.err.println("Success.");
-        } catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-    }
-    
-    public void initializeParseTrace(String parsedSentenceList)
-    {
-        
-    }
-    
-    public void registerStepSourceCodeLocation(int id, String absolutePath, int lineNumber)
-    {
-        
-    }
-    
-    public void registerRuleApplication(int id, int left, int right, String ruleName)
-    {
-        
-    }
-    
-    public void registerChartEdge(int number, int left, int right, String ruleName)
-    {
-        
-    }
-    
-    public void registerEdgeDependency(int motherID, int daughterID)
-    {
-        
-    }
-    
-    public void registerMessageChunk(String chunk)
-    {
-        
-    }
-    
-    public void registerMessageEnd(int externalStepID, String type)
-    {
-        
-    }
-    
-    public void registerParseEnd()
-    {
-        
-    }
-    
-    public char getPressedButton()
-    {
-       return 'n';
-    }
-    
-    public void registerStepLocation(int externalStepID, int externalParentID)
-    {
-    	
-    }
-    
-    
+	public void start()
+	{
+		System.err.print("Trying to build GUI window... ");
+		try
+		{
+			// TODO Softcode choice between DbDataManager and MemDataManager.
+			kahina = new KahinaInstance(
+					new DbDataManager(new DatabaseHandler()));
+			control = new KahinaController();
+			gui = new KahinaGUI(kahina, control);
+			System.err.println("Success.");
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public void initializeParseTrace(String parsedSentenceList)
+	{
+
+	}
+
+	public void registerStepSourceCodeLocation(int id, String absolutePath,
+			int lineNumber)
+	{
+
+	}
+
+	public void registerRuleApplication(int id, int left, int right,
+			String ruleName)
+	{
+
+	}
+
+	public void registerChartEdge(int number, int left, int right,
+			String ruleName)
+	{
+
+	}
+
+	public void registerEdgeDependency(int motherID, int daughterID)
+	{
+
+	}
+
+	public void registerMessageChunk(String chunk)
+	{
+
+	}
+
+	public void registerMessageEnd(int externalStepID, String type)
+	{
+
+	}
+
+	public void registerParseEnd()
+	{
+
+	}
+
+	public char getPressedButton()
+	{
+		return 'n';
+	}
+
+	public void registerStepLocation(int externalStepID, int externalParentID)
+	{
+
+	}
+	
+	public void registerStepExit(int externalStepID)
+	{
+		
+	}
+	
+	public void registerStepFailure(int externalStepID)
+	{
+		
+	}
+	
+	public void registerStepFinished(int externalStepID)
+	{
+		
+	}
+	
+	public void registerStepRedo(int externalStepID)
+	{
+		
+	}
+
+	public void registerBlockedPseudoStepInformation(int externalStepID,
+			String goal)
+	{
+
+	}
+
+	public void registerUnblockedPseudoStepInformation(int externalStepID,
+			int externalBlockedPseudoStepID, String goal)
+	{
+
+	}
+
 }
