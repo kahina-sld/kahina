dc4c0839eff62a19f8f16198b43d4ef47bbc6eea
ke <ke@6e8e43ff-ab70-0410-9a54-f4433d99c037>
2010-09-14 18:14:30 +0000

    Fixed redo: frame IDs are not unique, they are reused after a frame fails. Now generating our own, unique and nicer, step IDs.

--- kahina/trunk/kahinaswi/kahinaswi.pl
+++ kahina/trunk/kahinaswi/kahinaswi.pl
@@ -4,11 +4,15 @@
 :- use_module(library(jpl)).
 
 :- dynamic bridge/1.
+:- dynamic frame_step/2.
+:- dynamic next_step/1.
 
 start(Bridge) :-
   jpl_call(class([org,kahina,prolog],['PrologDebuggerRunner']),runAndGetBridge,
       [],Bridge),
-  assert(bridge(Bridge)).
+  assert(bridge(Bridge)),
+  retractall(next_step(_)), % TODO more systematic cleanup
+  assert(next_step(1)).
 
 get_bridge(Bridge) :-
   bridge(Bridge),
@@ -18,34 +22,48 @@ get_bridge(Bridge) :-
 
 user:prolog_trace_interception(Port,Frame,_Choice,Action) :-
   % TODO check if is Kahina-SWI is switched on, there should be some flag.
-  get_bridge(Bridge),
-  !,
+  % TODO what's with notrace?
+  notrace((get_bridge(Bridge),
   act(Port,Frame,Bridge),
   get_gui_action(Bridge,GUIAction),
   fix_action(Port,GUIAction,FixedGUIAction),
-  map_action(FixedGUIAction,_,Action).
+  map_action(FixedGUIAction,_,Action))).
 
 act(call,Frame,Bridge) :-
-  % TODO use own, nicer IDs
-  get_parent_id(Frame,ParentID),
+  %log(call,Frame),
+  get_next_step(Step),
+  assert(frame_step(Frame,Step)),
+  get_parent_step(Frame,ParentStep),
   prolog_frame_attribute(Frame,goal,Goal),
   term_to_atom(Goal,GoalAtom), % TODO shorten, extra view for full goals
-  jpl_call(Bridge,step,[Frame,GoalAtom],_),
-  jpl_call(Bridge,call,[Frame,ParentID],_).
+  jpl_call(Bridge,step,[Step,GoalAtom],_),
+  jpl_call(Bridge,call,[Step,ParentStep],_).
 act(fail,Frame,Bridge) :-
-  jpl_call(Bridge,fail,[Frame],_).
+  %log(fail,Frame),
+  retract(frame_step(Frame,Step)),
+  jpl_call(Bridge,fail,[Step],_).
 act(exit,Frame,Bridge) :-
+  %log(exit,Frame),
+  frame_step(Frame,Step),
   prolog_frame_attribute(Frame,has_alternatives,HasAlternatives),
   not(HasAlternatives,Deterministic),
-  jpl_call(Bridge,exit,[Frame,@(Deterministic)],_).
+  jpl_call(Bridge,exit,[Step,@(Deterministic)],_).
 act(redo,Frame,Bridge) :-
-  jpl_call(Bridge,redo,[Frame]).
+  %log(redo,Frame),
+  frame_step(Frame,Step),
+  jpl_call(Bridge,redo,[Step],_).
 % TODO exception handling
 
-get_parent_id(Frame,ParentID) :-
-  prolog_frame_attribute(Frame,parent,ParentID),
+get_next_step(Step) :-
+  retract(next_step(Step)),
+  NewStep is Step + 1,
+  assert(next_step(NewStep)).
+
+get_parent_step(Frame,ParentStep) :-
+  prolog_frame_attribute(Frame,parent,ParentFrame),
+  frame_step(ParentFrame,ParentStep),
   !.
-get_parent_id(_,-1).
+get_parent_step(_,-1).
 
 get_gui_action(Bridge,GUIAction) :-
   repeat,
@@ -62,3 +80,8 @@ char_command(115,skip). % s
 char_command(99,creep). % c
 char_command(102,fail). % f
 char_command(97,abort). % a
+
+log(Port,Frame) :-
+  prolog_frame_attribute(Frame,pc,PC),
+  write(log(Port,Frame,pc:PC)),
+  nl.
