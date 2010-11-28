cf6009eed47959963b0fa766bb3b4f74fd37f6f1
jd <jd@6e8e43ff-ab70-0410-9a54-f4433d99c037>
2010-11-28 10:02:13 +0000

    Enable layered tree view for Prolog mode. First version tailored towards graph example.

--- kahina/trunk/src/org/kahina/prolog/gui/PrologGUI.java
+++ kahina/trunk/src/org/kahina/prolog/gui/PrologGUI.java
@@ -1,9 +1,11 @@
 package org.kahina.prolog.gui;
 
 import org.kahina.core.control.KahinaController;
+import org.kahina.core.visual.tree.KahinaLayeredTreeView;
 import org.kahina.lp.gui.LogicProgrammingGUI;
 import org.kahina.prolog.PrologDebuggerInstance;
 import org.kahina.prolog.PrologStep;
+import org.kahina.prolog.data.tree.PrologLayerDecider;
 
 public class PrologGUI extends LogicProgrammingGUI
 {
@@ -12,5 +14,19 @@ public class PrologGUI extends LogicProgrammingGUI
 	{
 		super(stepType, instance, control);
 	}
+	
+	@Override
+	protected KahinaLayeredTreeView generateTreeView(KahinaController control)
+	{
+		return new KahinaLayeredTreeView(control, 0, 1, 2);
+	}
 
+	@Override
+	public void displayMainViews()
+	{
+		super.displayMainViews();
+		//only set deciders here because the trees are generated generically by the KahinaState
+		mainTreeView.getModel().setLayerDecider(new PrologLayerDecider(2));
+		mainTreeView.getSecondaryModel().setLayerDecider(new PrologLayerDecider(2));
+	}
 }
