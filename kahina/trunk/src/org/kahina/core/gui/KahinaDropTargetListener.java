6e9f8ff8b5cc27eec1ecba17e99cabd5f23fa780
jd <jd@6e8e43ff-ab70-0410-9a54-f4433d99c037>
2011-05-28 22:57:30 +0000

    Prevented main window from being embedded; adapted default perspectives.

--- kahina/trunk/src/org/kahina/core/gui/KahinaDropTargetListener.java
+++ kahina/trunk/src/org/kahina/core/gui/KahinaDropTargetListener.java
@@ -49,57 +49,64 @@ public class KahinaDropTargetListener implements DropTargetListener
                 e.acceptDrop(DnDConstants.ACTION_MOVE);
 
                 int winID = Integer.parseInt((String) tr.getTransferData(DataFlavor.stringFlavor));
-                
-                System.err.println("Moved window " + winID);
-                
-                KahinaRunner.processEvent(new KahinaWindowEvent(KahinaWindowEventType.UNDOCK, winID)); 
-                if (w.isDummyWindow())
+                if (winID != w.wm.arr.mainWindowID)
                 {
-	                if (!w.isTopLevelWindow())
+                
+	                System.err.println("Moved window " + winID);
+	                
+	                KahinaRunner.processEvent(new KahinaWindowEvent(KahinaWindowEventType.UNDOCK, winID)); 
+	                if (w.isDummyWindow())
 	                {
-	                	KahinaWindow embWin = w.getEmbeddingWindow();
-	                	embWin.replaceSubwindow(w, w.wm.getWindowByID(winID));
-	                	w.wm.arr.setEmbeddingWindowID(winID, embWin.getID());
-	                	embWin.validate();
-	                	embWin.repaint(); 
-	                    KahinaRunner.processEvent(new KahinaWindowEvent(KahinaWindowEventType.REMOVE, winID));
+		                if (!w.isTopLevelWindow())
+		                {
+		                	KahinaWindow embWin = w.getEmbeddingWindow();
+		                	embWin.replaceSubwindow(w, w.wm.getWindowByID(winID));
+		                	w.wm.arr.setEmbeddingWindowID(winID, embWin.getID());
+		                	embWin.validate();
+		                	embWin.repaint(); 
+		                    KahinaRunner.processEvent(new KahinaWindowEvent(KahinaWindowEventType.REMOVE, winID));
+		                }
+		                else
+		                {
+		                	KahinaRunner.processEvent(new KahinaWindowEvent(KahinaWindowEventType.REMOVE, w.getID()));
+		                	//move positions of new window to simulate replacement
+		                	KahinaWindow newWindow = w.wm.getWindowByID(winID);
+		                	newWindow.setLocation(w.getLocation());
+		                	newWindow.validate();
+		                	newWindow.repaint();
+		                }
 	                }
 	                else
 	                {
-	                	KahinaRunner.processEvent(new KahinaWindowEvent(KahinaWindowEventType.REMOVE, w.getID()));
-	                	//move positions of new window to simulate replacement
-	                	KahinaWindow newWindow = w.wm.getWindowByID(winID);
-	                	newWindow.setLocation(w.getLocation());
-	                	newWindow.validate();
-	                	newWindow.repaint();
+	                	switch (w.getWindowType())
+	                	{
+	                		case KahinaWindowType.LIST_WINDOW:
+	                		{
+	                			KahinaListWindow listWindow = (KahinaListWindow) w;
+	                			listWindow.addSubwindow(w.wm.getWindowByID(winID));
+	                			break;
+	                		}
+	                		case KahinaWindowType.TABBED_WINDOW:
+	                		{
+	                			KahinaTabbedWindow tabbedWindow = (KahinaTabbedWindow) w;
+	                			tabbedWindow.addSubwindow(w.wm.getWindowByID(winID));
+	                			break;
+	                		}
+	                		case KahinaWindowType.MAIN_WINDOW:
+	                		{
+	                			KahinaMainWindow mainWindow = (KahinaMainWindow) w;
+	                			mainWindow.addSubwindow(w.wm.getWindowByID(winID));
+	                			break;
+	                		}
+	                	}
+	                	KahinaRunner.processEvent(new KahinaWindowEvent(KahinaWindowEventType.REMOVE, winID));
 	                }
                 }
                 else
                 {
-                	switch (w.getWindowType())
-                	{
-                		case KahinaWindowType.LIST_WINDOW:
-                		{
-                			KahinaListWindow listWindow = (KahinaListWindow) w;
-                			listWindow.addSubwindow(w.wm.getWindowByID(winID));
-                			break;
-                		}
-                		case KahinaWindowType.TABBED_WINDOW:
-                		{
-                			KahinaTabbedWindow tabbedWindow = (KahinaTabbedWindow) w;
-                			tabbedWindow.addSubwindow(w.wm.getWindowByID(winID));
-                			break;
-                		}
-                		case KahinaWindowType.MAIN_WINDOW:
-                		{
-                			KahinaMainWindow mainWindow = (KahinaMainWindow) w;
-                			mainWindow.addSubwindow(w.wm.getWindowByID(winID));
-                			break;
-                		}
-                	}
-                	KahinaRunner.processEvent(new KahinaWindowEvent(KahinaWindowEventType.REMOVE, winID));
+                	System.err.println("Cannot embed main window!");
                 }
-                e.getDropTargetContext().dropComplete(true);          
+	            e.getDropTargetContext().dropComplete(true);          
             } 
             else 
             {
