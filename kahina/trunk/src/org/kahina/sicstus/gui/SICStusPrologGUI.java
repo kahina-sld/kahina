4bac226f7c4c07f2c8a99057f78239fd11afb9e5
jd <jd@6e8e43ff-ab70-0410-9a54-f4433d99c037>
2011-05-29 15:23:34 +0000

    Adapted kahinasictus to structural changes, defined fully integrated perspective.

--- kahina/trunk/src/org/kahina/sicstus/gui/SICStusPrologGUI.java
+++ kahina/trunk/src/org/kahina/sicstus/gui/SICStusPrologGUI.java
@@ -1,14 +1,22 @@
 package org.kahina.sicstus.gui;
 
 import org.kahina.core.control.KahinaController;
+import org.kahina.core.gui.KahinaGUI;
+import org.kahina.core.gui.KahinaWindowManager;
 import org.kahina.lp.gui.LogicProgrammingGUI;
 import org.kahina.sicstus.SICStusPrologDebuggerInstance;
 import org.kahina.sicstus.SICStusPrologStep;
+import org.kahina.tralesld.gui.TraleSLDWindowManager;
 
 public class SICStusPrologGUI extends LogicProgrammingGUI
 {
 	public SICStusPrologGUI(Class<? extends SICStusPrologStep> stepType, SICStusPrologDebuggerInstance instance, KahinaController control)
 	{
 		super(stepType, instance, control);
+	}
+	
+	protected KahinaWindowManager createWindowManager(KahinaGUI gui, KahinaController control)
+	{
+		return new SICStusPrologWindowManager(gui, control);
 	}
 }
