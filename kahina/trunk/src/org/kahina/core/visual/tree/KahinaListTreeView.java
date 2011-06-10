a767133469246d3867b9c26bbc834ab217e23b6b
jd <jd@6e8e43ff-ab70-0410-9a54-f4433d99c037>
2011-06-10 09:03:01 +0000

    Added most tree view options to KahinaListTreeView; some will become irrelevant.

--- kahina/trunk/src/org/kahina/core/visual/tree/KahinaListTreeView.java
+++ kahina/trunk/src/org/kahina/core/visual/tree/KahinaListTreeView.java
@@ -1,10 +1,19 @@
 package org.kahina.core.visual.tree;
 
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Stroke;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 
 import javax.swing.JComponent;
 
 import org.kahina.core.control.KahinaController;
+import org.kahina.core.data.tree.KahinaMemTree;
 import org.kahina.core.data.tree.KahinaTree;
 import org.kahina.core.gui.event.KahinaUpdateEvent;
 import org.kahina.core.visual.KahinaView;
@@ -13,29 +22,188 @@ import org.kahina.core.visual.KahinaViewConfiguration;
 public class KahinaListTreeView extends KahinaView<KahinaTree>
 {
 	private static final boolean VERBOSE = false;
+	
+	KahinaTreeViewConfiguration config;
+	
 	private final int[] layers;
-	private KahinaTree secondaryModel;
-	int selectedStep;
+	private KahinaTree secondaryTreeModel;
+	
+	// special display properties for certain nodes
+	HashMap<Integer, Color> nodeBorderColor;
+	HashMap<Integer, Boolean> statusDisplayed;
+
+	// mapping from status values to display properties
+	HashMap<Integer, Color> statusNodeColorEncoding;
+	HashMap<Integer, Color> statusEdgeColorEncoding;
+	HashMap<Integer, Color> statusBorderColorEncoding;
+	HashMap<Integer, Stroke> statusStrokeEncoding;
+	HashMap<Integer, Font> statusFontEncoding;
+	HashMap<Integer, Boolean> statusVisibilityEncoding;
+	
+	// allow marking of a single node in the tree
+	private int markedNode;
 	
 	public KahinaListTreeView(KahinaController control, int... layers)
 	{
 		super(control);
 		this.layers = layers;
+		model = new KahinaMemTree();
+		secondaryTreeModel = null;
+		resetAllStructures();
+		nodeBorderColor = new HashMap<Integer, Color>();
+		statusNodeColorEncoding = new HashMap<Integer, Color>();
+		statusEdgeColorEncoding = new HashMap<Integer, Color>();
+		statusBorderColorEncoding = new HashMap<Integer, Color>();
+		statusStrokeEncoding = new HashMap<Integer, Stroke>();
+		statusFontEncoding = new HashMap<Integer, Font>();
+		statusVisibilityEncoding = new HashMap<Integer, Boolean>();
+		
+		config = new KahinaTreeViewConfiguration();
+
+		markedNode = -1;
+	}	
+
+	public void display(KahinaTree treeModel)
+	{
+		model = treeModel;
+		nodeBorderColor = new HashMap<Integer, Color>();
+		if (VERBOSE)
+		{
+			System.err.println("Recalculating...");
+		}
+		recalculate(); // TODO is this necessary?
+		if (VERBOSE)
+		{
+			System.err.println("Recalculated.");
+		}
 	}
 
 	public void displaySecondaryTree(KahinaTree treeModel)
 	{
-		if (VERBOSE)
-		{
-			System.err.println(this + ".displaySecondaryTree(" + treeModel + ")");
-		}
-		this.secondaryModel = treeModel;
+		this.secondaryTreeModel = treeModel;
+		(this.secondaryTreeModel).setReferenceNode(model.getReferenceNode());
+		this.secondaryTreeModel.setPrimaryModel(model);
+		nodeBorderColor = new HashMap<Integer, Color>();
+		recalculate(); // TODO is this necessary?
+	}
+	
+	public void display(KahinaTree layerModel, int referenceNode)
+	{
+		layerModel.setReferenceNode(referenceNode);
+		model = layerModel;
+		nodeBorderColor = new HashMap<Integer, Color>();
+		recalculate();
+	}
+	
+	public KahinaTreeViewConfiguration getConfig()
+	{
+		return config;
+	}
+	
+	public void setConfig(KahinaTreeViewConfiguration config)
+	{
+		this.config = config;
+	}
+	
+	public boolean isSecondDimensionDisplayed()
+	{
+		return config.isSecondDimensionDisplayed() && secondaryTreeModel != null;
 	}
 
-	public KahinaTree getSecondaryModel()
+	public Font getNodeFont(int nodeID)
 	{
-		return secondaryModel;
+		int status = model.getNodeStatus(nodeID);
+		Font fnt = statusFontEncoding.get(status);
+		if (fnt == null)
+		{
+			if (model.isCollapsed(nodeID) && config.getCollapsePolicy() == KahinaTreeViewOptions.COLLAPSE_PRIMARY)
+			{
+				return new Font(Font.SANS_SERIF, Font.BOLD, config.getZoomLevel());
+			}
+			if (secondaryTreeModel != null && secondaryTreeModel.isCollapsed(nodeID) && config.getCollapsePolicy() == KahinaTreeViewOptions.COLLAPSE_SECONDARY)
+			{
+				return new Font(Font.SANS_SERIF, Font.BOLD, config.getZoomLevel());
+			}
+			return new Font(Font.SANS_SERIF, Font.PLAIN, config.getZoomLevel());
+		} else
+		{
+			return new Font(fnt.getFamily(), fnt.getStyle(), config.getZoomLevel());
+		}
 	}
+
+	public Color getNodeColor(int nodeID)
+	{
+		int status = model.getNodeStatus(nodeID);
+		Color col = statusNodeColorEncoding.get(status);
+		if (VERBOSE)
+		{
+			System.err.println("Node " + nodeID + ": status " + status + " ->  color " + col);
+		}
+		if (col == null)
+		{
+			return Color.WHITE;
+		} else
+		{
+			return col;
+		}
+	}
+
+	public void setNodeBorderColor(int nodeID, Color color)
+	{
+		if (color == null)
+		{
+			nodeBorderColor.remove(nodeID);
+		} else
+		{
+			nodeBorderColor.put(nodeID, color);
+		}
+	}
+
+	public Color getNodeBorderColor(int nodeID)
+	{
+		return nodeBorderColor.get(nodeID);
+	}
+	
+	/**
+	 * Returns the edge style for the edge leading to a node.
+	 * At the moment, this is a constant since dotted lines are not yet implemented.
+	 * @param nodeID
+	 * @return KahinaTreeViewOptions.COMPLETE_LINES
+	 */
+	public int getEdgeStyle(int nodeID)
+	{
+		return KahinaTreeViewOptions.COMPLETE_LINES;
+	}
+	
+	public void setStatusColorEncoding(int status, Color color)
+	{
+		if (VERBOSE)
+		{
+			System.err.println("status " + status + " -> color " + color);
+		}
+		statusNodeColorEncoding.put(status, color);
+	}
+
+	public void setStatusFontEncoding(int status, Font font)
+	{
+		statusFontEncoding.put(status, font);
+	}
+
+	public int getMarkedNode()
+	{
+		return markedNode;
+	}
+
+	public void setMarkedNode(int markedNode)
+	{
+		this.markedNode = markedNode;
+	}
+
+	public void resetAllStructures()
+	{
+		statusDisplayed = new HashMap<Integer, Boolean>();
+	}
+
 
 	@Override
 	public JComponent wrapInPanel(KahinaController control)
@@ -46,15 +214,96 @@ public class KahinaListTreeView extends KahinaView<KahinaTree>
 		return panel;
 	}
 
-	public void selectStep(int stepID)
+	public boolean nodeIsVisible(int nodeID)
 	{
-		selectedStep = stepID;
+		if (VERBOSE)
+		{
+			System.err.println(this + ".nodeIsVisible(" + nodeID + ")");
+		}
+		// TODO: process conditional option, allow user-definable decision function
+		if (config.getNodeDisplayPolicy() == KahinaTreeViewOptions.ALWAYS) return true;
+		if (config.getNodeDisplayPolicy() == KahinaTreeViewOptions.NEVER) return false;
+		if (VERBOSE)
+		{
+			System.err.println("Looking for collapsed ancestor...");
+		}
+		if (secondaryTreeModel != null && config.getCollapsePolicy() == KahinaTreeViewOptions.COLLAPSE_SECONDARY && secondaryTreeModel.hasCollapsedAncestor(nodeID)) return false;
+		int status = model.getNodeStatus(nodeID);
+		Boolean decision = statusVisibilityEncoding.get(status);
+		if (decision == null)
+		{
+			// default values decide
+			if (config.getNodeDisplayPolicy() == KahinaTreeViewOptions.STATUS_DEFAULT_YES) return true;
+			if (config.getNodeDisplayPolicy() == KahinaTreeViewOptions.STATUS_DEFAULT_NO) return false;
+		}
+		if (VERBOSE)
+		{
+			System.err.println("//" + this + ".nodeIsVisible(" + nodeID + "): " + decision);
+		}
+		return decision;
 	}
 
-	@Override
-	protected void processEvent(KahinaUpdateEvent e)
+	private int getVisibleParent(int nodeID, int treeLayer)
 	{
-		// recalculation is implicitly part of this (via marker)
-		selectStep(e.getSelectedStep());
+		int parent = model.getParent(nodeID, treeLayer);
+		while (!nodeIsVisible(parent))
+		{
+			parent = model.getParent(parent, treeLayer);
+		}
+		return parent;
+	}
+
+	private int getVisibleSecondaryParent(int nodeID, int treeLayer)
+	{
+		int secondaryParent = secondaryTreeModel.getParent(nodeID, treeLayer);
+		while (!nodeIsVisible(secondaryParent))
+		{
+			secondaryParent = secondaryTreeModel.getParent(secondaryParent, treeLayer);
+		}
+		return secondaryParent;
+	}
+
+	private ArrayList<Integer> getVisibleVirtualChildren(KahinaTree treeModel, int nodeID, int treeLayer)
+	{
+		ArrayList<Integer> descendants = new ArrayList<Integer>();
+		// System.err.println("\t Actual children for node " + nodeID + ": " +
+		// treeModel.getChildren(nodeID,treeLayer));
+		if (treeModel.isCollapsed(nodeID) && config.getCollapsePolicy() == KahinaTreeViewOptions.COLLAPSE_PRIMARY)
+			return descendants;
+		descendants.addAll(treeModel.getChildren(nodeID, treeLayer, true));
+		if (VERBOSE)
+		{
+			System.err.println("Breadth-first search for visible descendants of " + nodeID + ":");
+		}
+		for (int i = 0; i < descendants.size(); i++)
+		{
+			if (VERBOSE)
+			{
+				System.err.println("i == " + i + ", descendants: " + descendants);
+			}
+			boolean nodeIsVisible = nodeIsVisible(descendants.get(i));
+			if (VERBOSE)
+			{
+				System.err.print(descendants.get(i));
+				System.err.print(nodeIsVisible ? "+" : "-");
+			}
+			if (!nodeIsVisible)
+			{
+				List<Integer> x = treeModel.getChildren(descendants.remove(i), treeLayer, true);
+				descendants.addAll(x);
+				i--;
+			}
+		}
+		if (VERBOSE)
+		{
+			System.err.println();
+			System.err.println("Virtual children for node " + nodeID + ": " + descendants);
+		}
+		return descendants;
+	}
+	
+	public KahinaTree getTreeModel()
+	{
+		return model;
 	}
 }
