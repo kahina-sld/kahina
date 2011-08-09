4d0d1b8881a3317a5d6f55de2bd7c302aa621817
jd <jd@6e8e43ff-ab70-0410-9a54-f4433d99c037>
2011-08-09 21:18:16 +0000

    Added Kahina data type for TRALE signatures.

---
+++ src/org/kahina/tralesld/data/signature/TraleSLDSignature.java
@@ -0,0 +1,41 @@
+package org.kahina.tralesld.data.signature;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.kahina.core.data.KahinaObject;
+
+public class TraleSLDSignature extends KahinaObject
+{
+	//types can simply be stored as strings because they have unique names
+	List<String> types;
+	
+	//the immediate subtypes and supertypes for each type are stored here
+	//this also implicitly encodes the sibling relation used by the signature-enhanced editor
+	Map<String,List<String>> subtypes;
+	Map<String,List<String>> supertypes;
+	
+	//store the features introduced at each type
+	//inherited features are not listed, except when a type restriction is tightened
+	Map<String,Map<String,String>> introFeats;
+	
+	public TraleSLDSignature()
+	{
+		//the empty signature contains the root type "bot" without features
+		types = new LinkedList<String>();
+		types.add("bot");
+		subtypes = new HashMap<String,List<String>>();
+		subtypes.put("bot", new LinkedList<String>());
+		supertypes = new HashMap<String,List<String>>();
+		supertypes.put("bot", new LinkedList<String>());
+		introFeats = new HashMap<String,Map<String,String>>();
+		introFeats.put("bot", new HashMap<String,String>());
+	}
+	
+	public static TraleSLDSignature importFromTrale()
+	{
+		return new TraleSLDSignature();
+	}
+}
