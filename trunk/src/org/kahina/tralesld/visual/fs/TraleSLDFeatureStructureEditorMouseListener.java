f6021a19a1d18e91ee840ef539d3ab5dca3b077f
jd <jd@6e8e43ff-ab70-0410-9a54-f4433d99c037>
2011-10-07 00:05:51 +0000

    Added some test code in preparation for basic editing functionality.

--- trunk/src/org/kahina/tralesld/visual/fs/TraleSLDFeatureStructureEditorMouseListener.java
+++ trunk/src/org/kahina/tralesld/visual/fs/TraleSLDFeatureStructureEditorMouseListener.java
@@ -1,6 +1,9 @@
 package org.kahina.tralesld.visual.fs;
 
 import gralej.blocks.BlockPanel;
+import gralej.om.IEntity;
+import gralej.om.IType;
+import gralej.om.ITypedFeatureStructure;
 
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -18,7 +21,21 @@ public class TraleSLDFeatureStructureEditorMouseListener implements MouseListene
 	public void mouseClicked(MouseEvent arg0) 
 	{
 		//System.err.println("Editor clicked at position (" + arg0.getX() + "," + arg0.getY() + ")");
-		System.err.println("Selected entity: " + blockPanel.getSelectedBlock().getModel().text());
+		if (blockPanel.getSelectedBlock() != null)
+		{
+			IEntity selectedEntity = blockPanel.getSelectedBlock().getModel();
+			System.err.println("Selected entity: " + selectedEntity.getClass().getCanonicalName() + " " + selectedEntity.text());
+			if (selectedEntity instanceof IType)
+			{
+				IType selectedType = (IType) selectedEntity;
+				System.err.println("  is of type " + selectedType.typeName());
+			}
+			else if (selectedEntity instanceof ITypedFeatureStructure)
+			{
+				ITypedFeatureStructure selectedFS = (ITypedFeatureStructure) selectedEntity;
+				System.err.println("  is a FS of type " + selectedFS.type().typeName());
+			}
+		}
 	}
 
 	@Override
