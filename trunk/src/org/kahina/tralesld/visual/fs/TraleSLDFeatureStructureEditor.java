36c42de39c6380180c9263f89ad7597aedf8fead
jd <jd@6e8e43ff-ab70-0410-9a54-f4433d99c037>
2011-10-13 21:53:16 +0000

    Flexibilized and prepared infrastructure for type manipulation.

--- trunk/src/org/kahina/tralesld/visual/fs/TraleSLDFeatureStructureEditor.java
+++ trunk/src/org/kahina/tralesld/visual/fs/TraleSLDFeatureStructureEditor.java
@@ -1,10 +1,14 @@
 package org.kahina.tralesld.visual.fs;
 
 import gralej.blocks.BlockPanel;
+import gralej.om.IEntity;
+import gralej.om.IType;
+import gralej.om.ITypedFeatureStructure;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseListener;
+import java.util.Set;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -25,12 +29,22 @@ public class TraleSLDFeatureStructureEditor extends TraleSLDFeatureStructureView
 	TraleSLDState state;
 	TraleSLDSignature sig;
 	
+	IEntity contextStructure;
+	String contextStructureType;
+	
 	public TraleSLDFeatureStructureEditor(TraleSLDState state)
 	{
 		super();
 		blockPanel = null;
 		this.state = state;
 		this.sig = state.getSignature();
+		this.contextStructure = null;
+		this.contextStructureType = "?";
+	}
+	
+	public String getContextStructureType()
+	{
+		return contextStructureType;
 	}
 	
 	@Override
@@ -53,6 +67,41 @@ public class TraleSLDFeatureStructureEditor extends TraleSLDFeatureStructureView
 		}
 		innerPanel.repaint();
 	}
+	
+	public void processContextStructure(IEntity entity)
+	{
+		this.contextStructure = entity;
+		if (entity instanceof IType)
+		{
+			IType selectedType = (IType) entity;
+			contextStructureType = selectedType.typeName();
+		}
+		else if (entity instanceof ITypedFeatureStructure)
+		{
+			ITypedFeatureStructure selectedFS = (ITypedFeatureStructure) entity;
+			contextStructureType = selectedFS.type().typeName();
+		}
+	}
+	
+	public Set<String> getContextSubtypes()
+	{
+		return sig.getSubtypes(contextStructureType);
+	}
+	
+	public Set<String> getContextSupertypes()
+	{
+		return sig.getSupertypes(contextStructureType);
+	}
+	
+	public Set<String> getContextSiblingTypes()
+	{
+		return sig.getSiblingTypes(contextStructureType);
+	}
+	
+	public TraleSLDFeatureStructureEditorMenu createContextMenu()
+	{
+		return new TraleSLDFeatureStructureEditorMenu(this, getContextSubtypes(), getContextSupertypes(), getContextSiblingTypes());
+	}
 
 	@Override
 	//only type of action at the moment are the type manipulation instructions from context menu
@@ -61,5 +110,16 @@ public class TraleSLDFeatureStructureEditor extends TraleSLDFeatureStructureView
 	{
 		String type = e.getActionCommand();
 		//TODO: take the cached structure and switch its type; adapt structure accordingly
+		if (contextStructure instanceof IType)
+		{
+			IType selectedType = (IType) contextStructure;
+			contextStructureType = selectedType.typeName();
+		}
+		else if (contextStructure instanceof ITypedFeatureStructure)
+		{
+			ITypedFeatureStructure selectedFS = (ITypedFeatureStructure) contextStructure;
+			//TODO: find out how to generate a good type
+			selectedFS.setType(null);
+		}
 	}
 }
