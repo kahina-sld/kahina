911affdd8c0563be1543e42986d21ef1e9658cb4
jd <jd@6e8e43ff-ab70-0410-9a54-f4433d99c037>
2011-10-17 16:23:22 +0000

    Workbench content management now less of a stub.

--- src/org/kahina/tralesld/visual/workbench/FeatureWorkbenchView.java
+++ src/org/kahina/tralesld/visual/workbench/FeatureWorkbenchView.java
@@ -1,8 +1,13 @@
 package org.kahina.tralesld.visual.workbench;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import javax.swing.JComponent;
 
 import org.kahina.core.control.KahinaController;
+import org.kahina.core.data.KahinaObject;
 import org.kahina.core.gui.KahinaGUI;
 import org.kahina.core.visual.KahinaView;
 import org.kahina.tralesld.TraleSLDState;
@@ -10,9 +15,28 @@ import org.kahina.tralesld.data.FeatureWorkbench;
 
 public class FeatureWorkbenchView extends KahinaView<FeatureWorkbench>
 {
+	private List<String> nameList;
+	
 	public FeatureWorkbenchView(KahinaController control)
 	{
 		super(control);
+	}
+	
+	public List<String> getNameList()
+	{
+		return nameList;
+	}
+	
+	public void doDisplay()
+	{
+		recalculate();
+	}
+	
+	public void recalculate()
+	{
+		nameList = new ArrayList<String>();
+		nameList.addAll(model.getNames());
+		Collections.sort(nameList);
 	}
 
 	@Override
