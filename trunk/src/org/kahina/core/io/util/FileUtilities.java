cb71da57a289e07e68ce8173ca300251c446a777
jd <jd@6e8e43ff-ab70-0410-9a54-f4433d99c037>
2011-11-09 17:48:46 +0000

    Enabled importing feature structures from GRISU files.

---
+++ trunk/src/org/kahina/core/io/util/FileUtilities.java
@@ -0,0 +1,21 @@
+package org.kahina.core.io.util;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileUtilities 
+{
+	public static String slurpFile(String file) throws IOException 
+	{
+	    BufferedReader reader = new BufferedReader( new FileReader (file));
+	    String line  = null;
+	    StringBuilder stringBuilder = new StringBuilder();
+	    String ls = System.getProperty("line.separator");
+	    while( ( line = reader.readLine() ) != null ) {
+	        stringBuilder.append( line );
+	        stringBuilder.append( ls );
+	    }
+	    return stringBuilder.toString();
+	 }
+}
