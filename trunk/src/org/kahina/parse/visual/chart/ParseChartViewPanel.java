8af517397ceabe35c494ea650a8679ce32786093
jd <jd@6e8e43ff-ab70-0410-9a54-f4433d99c037>
2012-01-08 18:58:19 +0000

    Added skeleton and stubs for org.kahina.parse package (specialized charts and grammar projects).

---
+++ trunk/src/org/kahina/parse/visual/chart/ParseChartViewPanel.java
@@ -0,0 +1,13 @@
+package org.kahina.parse.visual.chart;
+
+import org.kahina.core.visual.chart.KahinaChartViewPanel;
+
+public class ParseChartViewPanel extends KahinaChartViewPanel
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4358324550444966379L;
+
+}
---
+++ trunk/src/org/kahina/parse/visual/project/ProjectViewPanel.java
@@ -0,0 +1,11 @@
+package org.kahina.parse.visual.project;
+
+import org.kahina.core.visual.KahinaViewPanel;
+
+public class ProjectViewPanel extends KahinaViewPanel<ProjectView>
+{
+	public void updateDisplay() 
+	{
+	}
+
+}
