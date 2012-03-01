8388eda93dd5033c751df8fae8b2af1a19e01fd8
jd <jd@6e8e43ff-ab70-0410-9a54-f4433d99c037>
2012-03-01 22:43:24 +0000

    Every project now contains a breakpoint profile, started to extend the basic profile.

--- trunk/src/org/kahina/core/data/breakpoint/KahinaBreakpointProfile.java
+++ trunk/src/org/kahina/core/data/breakpoint/KahinaBreakpointProfile.java
@@ -1,6 +1,14 @@
 package org.kahina.core.data.breakpoint;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class KahinaBreakpointProfile 
 {
-
+	List<KahinaBreakpoint> treeBreakpoints;
+	
+	public KahinaBreakpointProfile()
+	{
+		treeBreakpoints = new ArrayList<KahinaBreakpoint>();
+	}
 }
