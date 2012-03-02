159a3e86dd8ca11f583db72ee5482836cde2c0f4
jd <jd@6e8e43ff-ab70-0410-9a54-f4433d99c037>
2012-03-02 12:17:52 +0000

    Special view variant for group SAT instances.

---
+++ trunk/src/org/kahina/logic/sat/visual/KahinaGroupSatInstanceView.java
@@ -0,0 +1,83 @@
+package org.kahina.logic.sat.visual;
+
+import java.awt.Color;
+import java.util.HashMap;
+
+import org.kahina.core.control.KahinaController;
+import org.kahina.core.visual.graph.KahinaGraphLayouter;
+import org.kahina.logic.sat.data.GroupCnfSatInstance;
+
+public class KahinaGroupSatInstanceView extends KahinaSatInstanceView
+{
+    GroupCnfSatInstance sat;
+    
+    public KahinaGroupSatInstanceView(KahinaController control, KahinaGraphLayouter layout)
+    {
+        super(control, layout);
+    }
+
+    public void display(GroupCnfSatInstance sat)
+    {
+        //do not recalculate if the sat instance is already displayed
+        if (this.sat == null || this.sat != sat)
+        {
+            this.sat = sat;
+            super.sat = sat;
+            displayClausesByVariables();
+        }
+    }
+    
+    public void displayGroupsByVariables()
+    {
+        model = sat.generateClaGroupByVarGraph();
+        if (!clauseGraph)
+        {
+            vertexBorderColor = new HashMap<Integer, Color>();
+            resetLayoutStructures();
+            layout.newGraph(this);
+        }
+        clauseGraph = true;
+    }
+    
+    public void displayGroupsByLiterals()
+    {
+        model = sat.generateClaGroupByLitGraph();
+        if (!clauseGraph)
+        {
+            vertexBorderColor = new HashMap<Integer, Color>();
+            resetLayoutStructures();
+            layout.newGraph(this);
+        }
+        clauseGraph = true;
+    }
+    
+    public void displayGroupsByComplementaryLiterals()
+    {
+        model = sat.generateClaGroupByCompLitGraph();
+        if (!clauseGraph)
+        {
+            vertexBorderColor = new HashMap<Integer, Color>();
+            resetLayoutStructures();
+            layout.newGraph(this);
+        }
+        clauseGraph = true;
+    }
+    
+    public void displayVariablesByGroups()
+    {
+        model = sat.generateVarByClaGroupGraph();
+        vertexBorderColor = new HashMap<Integer, Color>();
+        resetLayoutStructures();
+        layout.newGraph(this);
+        clauseGraph = false;
+    }
+    
+    public void displayLiteralsByGroups()
+    {
+        model = sat.generateLitByClaGroupGraph();
+        vertexBorderColor = new HashMap<Integer, Color>();
+        resetLayoutStructures();
+        layout.newGraph(this);
+        clauseGraph = false;
+    }
+}
