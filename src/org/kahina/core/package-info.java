19a4dba20d8476c63fab2121a63f718c5170eae4
jd <jd@6e8e43ff-ab70-0410-9a54-f4433d99c037>
2012-03-13 23:50:32 +0000

    Updated package documentation for org.kahina.core and org.kahina.core.data.chart.

--- src/org/kahina/core/package-info.java
+++ src/org/kahina/core/package-info.java
@@ -4,15 +4,12 @@
 * The root package includes the class {@link org.kahina.core.KahinaInstance}
 * which is the central piece of the architecture because it groups together
 * the step database, the controller, the bridge, and configuration information.
+* It also administers two instances of {@link org.kahina.core.control.KahinaController},
+* and allows other components to dispatch events through these controllers.
 * <p>
 * The configuration data are grouped together in a {@link org.kahina.core.KahinaState},
 * whereas the step information is stored in a collection of {@link org.kahina.core.KahinaStep} objects.
 * <p>
-* {@link org.kahina.core.KahinaRunner} gives static access to Kahina's event system
-* and the step database without a need to hand around a KahinaInstance object.
-* This part of the architecture is not yet stable,
-* and especially the functionality provided by {@link org.kahina.core.KahinaRunner}
-* is planned to be treated in a more systematic manner.
 * {@link org.kahina.core.KahinaException} is Kahina's basic exception datatype,
 * from which all specialized exception types in various parts of the system should inherit.
 * 
