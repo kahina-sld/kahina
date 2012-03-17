4038523827b5da93a5d72bcc9df6a9e748288f5b
jd <jd@6e8e43ff-ab70-0410-9a54-f4433d99c037>
2012-03-17 10:19:45 +0000

    Added TGF (Trivial Graph Format) input for KahinaGraphs.

--- trunk/src/org/kahina/core/data/graph/KahinaGraph.java
+++ trunk/src/org/kahina/core/data/graph/KahinaGraph.java
@@ -1,5 +1,10 @@
 package org.kahina.core.data.graph;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 import java.util.Set;
 
@@ -7,6 +12,15 @@ import org.kahina.core.data.KahinaObject;
 
 public abstract class KahinaGraph extends KahinaObject
 {
+    /**
+     * Creates an empty graph, an AdjacListsGraph by default.
+     * @return an empty KahinaGraph (implemented as an AdjacListsGraph by default)
+     */
+    public static KahinaGraph createEmptyGraph()
+    {
+        return new AdjacListsGraph();
+    }
+    
     public abstract Set<Integer> getVertices();
     
     public abstract void addVertex(int v);
@@ -37,4 +51,94 @@ public abstract class KahinaGraph extends KahinaObject
     
     public abstract void setEdgeStatus(int v1, int v2, int status);
     public abstract int getEdgeStatus(int v1, int v2);
+    
+    /**
+     * Imports a graph from a file in Trivial Graph Format (TGF).
+     * @param fileName path to the input file in TGF format
+     * @return a KahinaGraph object representing the graph from the file
+     */
+    public static KahinaGraph importTGF(String fileName)
+    {
+        KahinaGraph graph = createEmptyGraph();
+        try
+        {
+            BufferedReader in = new BufferedReader(new FileReader(fileName));
+            int phase = 0; //0 -> read vertex labels, 1 -> read edges and their labels
+            while (in.ready())
+            {
+                String line = in.readLine();
+                if (line.equals("#"))
+                {
+                    phase = 1;
+                }
+                else if (phase == 0)
+                {
+                    int firstSpace = line.indexOf(' ');
+                    try
+                    {
+                        if (firstSpace != -1)
+                        {
+                            String vertexID = line.substring(0, firstSpace);
+                            String vertexLabel = line.substring(firstSpace + 1);
+                            graph.addVertex(Integer.parseInt(vertexID), vertexLabel);
+                        }
+                        else
+                        {
+                            //assume empty label
+                            graph.addVertex(Integer.parseInt(line));
+                        }  
+                    }
+                    catch (NumberFormatException e)
+                    {
+                        System.err.println("ERROR during TGF input: Vertex line not starting with ID!");
+                        return graph;
+                    }
+                }
+                else if (phase == 1)
+                {
+                    int firstSpace = line.indexOf(' ');
+                    try
+                    {
+                        if (firstSpace != -1)
+                        {
+                            String v1ID = line.substring(0, firstSpace);
+                            String rest = line.substring(firstSpace + 1);
+                            int secondSpace = rest.indexOf(' ');
+                            if (secondSpace != -1)
+                            {
+                                String v2ID = rest.substring(0, secondSpace);
+                                String edgeLabel = rest.substring(secondSpace + 1);
+                                graph.addDirectedEdge(Integer.parseInt(v1ID), 
+                                                      Integer.parseInt(v2ID), edgeLabel);
+                            }
+                            else
+                            {
+                                //assume empty label
+                                graph.addDirectedEdge(Integer.parseInt(v1ID), Integer.parseInt(rest));
+                            }
+                        }
+                        else
+                        {
+                            System.err.println("ERROR during TGF input: Malformed edge line!");
+                            return graph;
+                        }  
+                    }
+                    catch (NumberFormatException e)
+                    {
+                        System.err.println("ERROR during TGF input: Edge line not starting with ID!");
+                        return graph;
+                    }
+                }
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            System.err.println("ERROR: Graph file not found. Returning empty graph!");
+        }
+        catch (IOException e)
+        {
+            System.err.println("ERROR: IOException while reading TGF file. Returning empty graph!");
+        }
+        return graph;
+    }
 }
