963c1733fbaedda78d27d76fb9c056286a275f87
ke <ke@6e8e43ff-ab70-0410-9a54-f4433d99c037>
2012-04-01 20:51:30 +0000

    Code cosmetics

--- trunk/src/org/kahina/core/gui/event/KahinaRedrawEvent.java
+++ trunk/src/org/kahina/core/gui/event/KahinaRedrawEvent.java
@@ -1,11 +1,12 @@
 package org.kahina.core.gui.event;
 
 import org.kahina.core.control.KahinaEvent;
+import org.kahina.core.control.KahinaEventTypes;
 
 public class KahinaRedrawEvent extends KahinaEvent
 {
     public KahinaRedrawEvent()
     {
-        super("redraw");
+        super(KahinaEventTypes.REDRAW);
     }
 }
