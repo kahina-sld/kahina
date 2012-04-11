3dacaffc173501442dc0ff227e6b54ecf7d75783
ke <ke@6e8e43ff-ab70-0410-9a54-f4433d99c037>
2012-04-11 15:58:21 +0000

    Added a "layer" field to KahinaSelectionEvents and KahinaUpdateEvents to indicate on what layer of a KahinaListTreeView the clicking causing the selection took place, if any, as the first step of an attempt to improve the usability of the list tree view in the presence of primary branching.

--- trunk/src/org/kahina/core/gui/event/KahinaSelectionEvent.java
+++ trunk/src/org/kahina/core/gui/event/KahinaSelectionEvent.java
@@ -7,41 +7,58 @@ import org.kahina.core.visual.KahinaViewPanel;
 /**
  * Event to indicate a certain step has been "marked" internally in the main
  * tree view and all views should be updated accordingly.
+ * 
  * @author ke
- *
+ * 
  */
 public class KahinaSelectionEvent extends KahinaEvent
 {
-    int selectedStep;
-    KahinaViewPanel<?> panel;
-    
-    public KahinaSelectionEvent(int selectedStep)
-    {
-        super(KahinaEventTypes.SELECTION);
-        this.selectedStep = selectedStep;
-        this.panel = null;
-    }
-    
-    public KahinaSelectionEvent(int selectedStep, KahinaViewPanel<?> panel)
-    {
-        super(KahinaEventTypes.SELECTION);
-        this.selectedStep = selectedStep;
-        this.panel = panel;
-    }
-    
-    public int getSelectedStep()
-    {
-        return selectedStep;
-    }
-    
-    public KahinaViewPanel<?> getPanel()
-    {
-        return panel;
-    }
-    
-    @Override
+	private final int selectedStep;
+
+	private final KahinaViewPanel<?> panel;
+
+	private final int layer;
+
+	public KahinaSelectionEvent(int selectedStep)
+	{
+		this(selectedStep, -1);
+	}
+	
+	public KahinaSelectionEvent(int selectedStep, int layer)
+	{
+		this(selectedStep, layer, null);
+	}
+
+	public KahinaSelectionEvent(int selectedStep, int layer, KahinaViewPanel<?> panel)
+	{
+		super(KahinaEventTypes.SELECTION);
+		this.selectedStep = selectedStep;
+		this.layer = layer;
+		this.panel = panel;
+	}
+
+	public int getSelectedStep()
+	{
+		return selectedStep;
+	}
+
+	/**
+	 * @return the layer of the GUI element that fired this event, or -1 if it
+	 *         was not fired by a GUI element associated with a specific layer
+	 */
+	public int getLayer()
+	{
+		return layer;
+	}
+
+	public KahinaViewPanel<?> getPanel()
+	{
+		return panel;
+	}
+
+	@Override
 	public String toString()
-    {
-        return  "select node " + selectedStep;
-    }
+	{
+		return "select node " + selectedStep;
+	}
 }
