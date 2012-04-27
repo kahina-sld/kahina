452302b4e9bbf737bca69e230e39b8bb753c94b5
jd <jd@6e8e43ff-ab70-0410-9a54-f4433d99c037>
2012-04-27 12:02:00 +0000

    Begun to extend DAG display by colored paths.

---
+++ src/org/kahina/core/data/dag/ColoredPath.java
@@ -0,0 +1,53 @@
+package org.kahina.core.data.dag;
+
+import java.awt.Color;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ColoredPath
+{
+    private Color color;
+    private LinkedList<Integer> path;
+    
+    /**
+     * Creates an empty path of the specified color.
+     * @param color the color of the new path
+     */
+    public ColoredPath(Color color)
+    {
+        this.color = color;
+        this.path = new LinkedList<Integer>();
+    }
+    
+    /**
+     * Extends the path by a numeric step ID.
+     * @param id the Id by which the path is extended
+     */
+    public void append(int id)
+    {
+        path.add(id);
+    }
+    
+    /**
+     * Gets the color of the path.
+     * @return a Color object representing the path's color
+     */
+    public Color getColor()
+    {
+        return color;
+    }
+    
+    /**
+     * Changes the color of the path.
+     * @param color a Color object representing the new color of the path
+     */
+    public void setColor(Color color)
+    {
+        this.color = color;
+    }
+    
+    public List<Integer> getPath()
+    {
+        return path;
+    }
+}
