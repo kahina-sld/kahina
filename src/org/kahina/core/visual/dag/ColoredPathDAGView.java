d92529af140057430443cc83e76bfb650b2e2a7f
jd <jd@6e8e43ff-ab70-0410-9a54-f4433d99c037>
2012-04-30 15:40:59 +0000

    Added rendering of multiple colored edges to DAG display.

--- src/org/kahina/core/visual/dag/ColoredPathDAGView.java
+++ src/org/kahina/core/visual/dag/ColoredPathDAGView.java
@@ -1,15 +1,22 @@
 package org.kahina.core.visual.dag;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import javax.swing.BoxLayout;
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
 import org.kahina.core.KahinaInstance;
 import org.kahina.core.data.dag.ColoredPath;
 import org.kahina.core.data.dag.ColoredPathDAG;
 import org.kahina.core.data.dag.KahinaDAG;
+import org.kahina.core.gui.KahinaProgressBar;
 
 public class ColoredPathDAGView extends KahinaDAGView
 {
@@ -20,7 +27,13 @@ public class ColoredPathDAGView extends KahinaDAGView
     public ColoredPathDAGView(KahinaInstance<?, ?, ?> kahina, KahinaDAGLayouter layout)
     {
         super(kahina, layout);
+        model = new ColoredPathDAG();
         edgeColors = new HashMap<Integer,List<Color>>();
+    }
+    
+    public ColoredPathDAG getModel()
+    {
+        return model;
     }
     
     public void display(ColoredPathDAG dagModel)
@@ -54,15 +67,34 @@ public class ColoredPathDAGView extends KahinaDAGView
     public void recalculate()
     {
         super.recalculate();
+        edgeColors.clear();
         for (ColoredPath colorPath : model.getColorPaths())
         {
             List<Integer> path = colorPath.getPath();
             for (int i = 0; i < path.size() - 1; i++)
             {
-                //TODO: implement getEdgeID
-                //int edgeID = model.getEdgeID(path.get(i), path.get(i+1));
-                //addEdgeColor(edgeID,colorPath.getColor());
+                int edgeID = model.getEdgeBetween(path.get(i), path.get(i+1));
+                addEdgeColor(edgeID,colorPath.getColor());
             }
         }
+    }
+    
+    @Override
+    public JComponent makePanel()
+    {
+        KahinaProgressBar progressBar = new KahinaProgressBar();
+        ColoredPathDAGViewPanel panel = new ColoredPathDAGViewPanel(kahina);
+        panel.setPreferredSize(new Dimension(200,300));
+        kahina.getGuiControl().registerListener("redraw", panel);
+        panel.setView(this);
+        JPanel scrollPaneAndProgressBar = new JPanel();
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.getViewport().setBackground(config.getBackgroundColor());
+        scrollPane.setViewportView(panel);
+        scrollPaneAndProgressBar.setLayout(new BoxLayout(scrollPaneAndProgressBar, BoxLayout.Y_AXIS));
+        scrollPaneAndProgressBar.add(scrollPane);
+        scrollPaneAndProgressBar.add(progressBar);
+        panel.setProgressBar(progressBar);
+        return scrollPaneAndProgressBar;
     }
 }
