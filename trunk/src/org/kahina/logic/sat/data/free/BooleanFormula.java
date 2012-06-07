9528def2315a6f63695fd5c10dc8f3575d5bd353
jd <jd@6e8e43ff-ab70-0410-9a54-f4433d99c037>
2012-06-07 17:18:18 +0000

    Each formula now has a flag to represent pruning, the Tseitin transformation ignores pruned subformulae.

--- trunk/src/org/kahina/logic/sat/data/free/BooleanFormula.java
+++ trunk/src/org/kahina/logic/sat/data/free/BooleanFormula.java
@@ -4,6 +4,19 @@ import org.kahina.logic.sat.io.free.BooleanFormulaVisitor;
 
 public abstract class BooleanFormula
 {
+    //used to represent pruned formula trees for selective Tseitin transformation
+    private boolean pruned = false;
+    
+    public boolean isPruned()
+    {
+        return pruned;
+    }
+    
+    public void setPruned(boolean pruned)
+    {
+        this.pruned = pruned;
+    }
+    
     public abstract String toStringWithMinimumBracing();
 
     public abstract int getSize();
