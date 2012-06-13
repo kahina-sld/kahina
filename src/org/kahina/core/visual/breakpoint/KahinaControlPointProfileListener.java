889ea29dd4e4f97c4f6fe3494cf1a50f4eaa8b8b
jd <jd@6e8e43ff-ab70-0410-9a54-f4433d99c037>
2012-06-13 22:10:43 +0000

    Adapted parts of the old breakpoint editor listener structure to new compact version. Work in progress.

--- src/org/kahina/core/visual/breakpoint/KahinaControlPointProfileListener.java
+++ src/org/kahina/core/visual/breakpoint/KahinaControlPointProfileListener.java
@@ -6,19 +6,47 @@ import java.awt.event.ActionListener;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
+import org.kahina.core.data.breakpoint.KahinaBreakpoint;
+import org.kahina.core.data.breakpoint.KahinaControlPoint;
+import org.kahina.core.data.breakpoint.patterns.TreeAutomaton;
+import org.kahina.core.edit.breakpoint.BreakpointEditorEvent;
+
 public class KahinaControlPointProfileListener implements ActionListener, ListSelectionListener
 {
-    KahinaControlPointProfileView profileView;
+    KahinaControlPointProfileViewPanel profilePanel;
     
-    public KahinaControlPointProfileListener(KahinaControlPointProfileViewPanel profileView)
+    public KahinaControlPointProfileListener(KahinaControlPointProfileViewPanel profilePanel)
     {
-        this.profileView = profileView.view;
+        this.profilePanel = profilePanel;
     }
 
-    public void actionPerformed(ActionEvent arg0)
+    public void actionPerformed(ActionEvent e)
     {
-        // TODO Auto-generated method stub
-        
+        String s = e.getActionCommand();
+        if (s.equals("newControlPoint"))
+        {
+            //TODO: adapt type argument, 0 is not an ideal default value!
+            KahinaControlPoint newControlPoint = new KahinaControlPoint(0);
+            profilePanel.view.getModel().addControlPoint(newControlPoint);
+            profilePanel.pointList.setListData(profilePanel.view.getModel().getControlPoints());
+            profilePanel.pointList.setSelectedIndex(profilePanel.view.getModel().getSize() - 1);
+        } 
+        //TODO: reactivate these parts!
+        /*else if (s.equals("activateBreakpoint"))
+        {
+            control.processEvent(new BreakpointEditorEvent(BreakpointEditorEvent.ACTIVATE_BREAKPOINT, curID));
+        } 
+        else if (s.equals("deactivateBreakpoint"))
+        {
+            control.processEvent(new BreakpointEditorEvent(BreakpointEditorEvent.DEACTIVATE_BREAKPOINT, curID));
+        } 
+        else if (s.equals("removeBreakpoint"))
+        {
+            control.processEvent(new BreakpointEditorEvent(BreakpointEditorEvent.REMOVE_BREAKPOINT, curID));
+        }*/
+        //TODO: new profile, import profile, save profile etc.
+        //TODO: deal with the activation status!
+        //adaptActivationStatus();
     }
 
     public void valueChanged(ListSelectionEvent arg0)
