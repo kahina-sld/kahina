38d886331fd28a0665d0464c64e053e4cdaaddb5
jd <jd@6e8e43ff-ab70-0410-9a54-f4433d99c037>
2012-06-20 16:09:06 +0000

    A daring measure to improve resizing behavior: all KahinaViewPanels now operate on a default scroll pane, with promising preliminary results.

--- src/org/kahina/core/visual/KahinaViewPanel.java
+++ src/org/kahina/core/visual/KahinaViewPanel.java
@@ -1,9 +1,12 @@
 package org.kahina.core.visual;
 
+import java.awt.Component;
 import java.lang.reflect.InvocationTargetException;
 
+import javax.swing.BoxLayout;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.SwingUtilities;
 
 import org.kahina.core.KahinaException;
@@ -19,9 +22,54 @@ public abstract class KahinaViewPanel<T extends KahinaView<?>> extends JPanel im
 	private static final boolean VERBOSE = false;
 
 	public T view;
+    
+    protected JScrollPane topScrollPane;
+    protected JPanel topScrollPanel;
 	
+    //a standardized progress bar for views which require extensive computations
     protected KahinaProgressBar progressBar;
     JComponent progressBarParent;
+    
+    public KahinaViewPanel()
+    {
+       topScrollPanel = new JPanel();
+       topScrollPanel.setLayout(new BoxLayout(topScrollPanel,BoxLayout.LINE_AXIS));
+       topScrollPane = new JScrollPane(topScrollPanel);
+       super.add(topScrollPane);
+    }
+    
+    //TODO: overload other methods inherited from JPanel as well, precluding unexpected behavior
+    
+    public Component add(Component comp)
+    {
+        return topScrollPanel.add(comp);
+    }
+    
+    public Component add(Component comp, int index)
+    {
+        return topScrollPanel.add(comp, index);
+    }
+    
+    public void add(Component comp, Object constraint)
+    {
+        topScrollPanel.add(comp, constraint);
+    }
+    
+    public void add(Component comp, Object constraint, int index)
+    {
+        topScrollPanel.add(comp, constraint);
+    }
+    
+    public void removeAll()
+    {
+        topScrollPanel.removeAll();
+    }
+    
+    public void revalidate()
+    {
+        super.revalidate();
+        if (topScrollPanel != null) topScrollPanel.revalidate();
+    }
 
 	public void processEvent(KahinaEvent event)
 	{
