b7998d2bee3f14cb2a7085afd4693212dc271a2c
jd <jd@6e8e43ff-ab70-0410-9a54-f4433d99c037>
2012-08-30 21:51:49 +0000

    Fixed a long-pending problem with controller treatment when re-initializing, which resulted in much undesired behavior.

--- trunk/src/org/kahina/lp/LogicProgrammingState.java
+++ trunk/src/org/kahina/lp/LogicProgrammingState.java
@@ -240,7 +240,6 @@ public class LogicProgrammingState extends KahinaState
     
     public void processEvent(KahinaEvent event)
     {
-        System.err.println("LogicProgrammingState.processNewAgentEvent(" + event + ")");
         super.processEvent(event);
         if (event instanceof NewControlAgentEvent)
         {
@@ -250,7 +249,6 @@ public class LogicProgrammingState extends KahinaState
     
     private void processNewAgentEvent(NewControlAgentEvent event)
     {
-        System.err.println("LogicProgrammingState.processNewAgentEvent(" + event + ")");
         KahinaControlPoint controlAgent = event.getControlAgent();
         switch (event.getAgentType())
         {
