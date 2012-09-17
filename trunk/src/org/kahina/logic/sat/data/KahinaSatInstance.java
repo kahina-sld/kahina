81d6526963399d6159866bf7bb11f88c25222cc1
jd <jd@6e8e43ff-ab70-0410-9a54-f4433d99c037>
2012-09-17 14:34:24 +0000

    Smybolic output for CNF instances, tool for DIMACS to symbolic DIMACS conversion.

--- trunk/src/org/kahina/logic/sat/data/KahinaSatInstance.java
+++ trunk/src/org/kahina/logic/sat/data/KahinaSatInstance.java
@@ -1,8 +1,36 @@
 package org.kahina.logic.sat.data;
 
+import java.util.HashMap;
+
 import org.kahina.core.data.KahinaObject;
 
 public class KahinaSatInstance extends KahinaObject
 {
-
+    HashMap<Integer,String> symbolTable;
+    
+    public KahinaSatInstance()
+    {
+        symbolTable = new HashMap<Integer,String>();
+    }
+    
+    public void setSymbolMapping(int id, String symbol)
+    {
+        symbolTable.put(id, symbol);
+    }
+    
+    public String getSymbolForLiteral(int literal)
+    {
+        if (Integer.signum(literal) == 1)
+        {
+            String symbol = symbolTable.get(literal);
+            if (symbol == null) return literal + "";
+            return symbol;
+        }
+        else
+        {
+            String symbol = symbolTable.get(-literal);
+            if (symbol == null) return literal + "";
+            return symbol;
+        }
+    }
 }
--- trunk/src/org/kahina/logic/sat/io/cnf/DimacsCnfOutput.java
+++ trunk/src/org/kahina/logic/sat/io/cnf/DimacsCnfOutput.java
@@ -31,4 +31,27 @@ public class DimacsCnfOutput
             e.printStackTrace();
         }
     }
+    
+    public static void writeSymbolDimacsCnfFile(String fileName, CnfSatInstance cnf)
+    {
+        try
+        {
+            FileWriter out = new FileWriter(new File(fileName));
+            out.write("p cnf " + cnf.getNumVars() + " " + cnf.getNumClauses() + "\n");
+            for (List<Integer> clause : cnf.getClauses())
+            {
+                for (Integer lit : clause)
+                {
+                    out.write(cnf.getSymbolForLiteral(lit) + " ");
+                }
+                out.write("0\n");
+            }
+            out.close();
+        }
+        catch (IOException e)
+        {
+            System.err.println("ERROR: Could not write to DIMACS CNF file: " + fileName);
+            e.printStackTrace();
+        }
+    }
 }
