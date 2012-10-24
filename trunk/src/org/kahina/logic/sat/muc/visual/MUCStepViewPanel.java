5f16125bf5886f23a4fb5414943d9f3ee1f21cf4
jd <jd@6e8e43ff-ab70-0410-9a54-f4433d99c037>
2012-10-24 17:54:00 +0000

    Filled UC view context menu, without functionality so far.

--- trunk/src/org/kahina/logic/sat/muc/visual/MUCStepViewPanel.java
+++ trunk/src/org/kahina/logic/sat/muc/visual/MUCStepViewPanel.java
@@ -6,11 +6,18 @@ import org.kahina.logic.sat.visual.cnf.list.KahinaSatInstanceListViewPanel;
 public class MUCStepViewPanel extends KahinaSatInstanceListViewPanel
 {
     MUCInstance kahina;
+    MUCStepView view;
     
     public MUCStepViewPanel(MUCInstance kahina)
     {
         super(kahina);
         this.kahina = kahina;
-        list.addMouseListener(new MUCStepViewPanelMouseListener(kahina));
+        list.addMouseListener(new MUCStepViewListener(kahina, this));
+    }
+    
+    public void setView(MUCStepView view)
+    {
+        super.setView(view);
+        this.view = view;
     }
 }
