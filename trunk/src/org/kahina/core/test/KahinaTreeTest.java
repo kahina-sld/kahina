1563bbd9b4a96186aed9d9a849be682ea972feb3
jd <jd@6e8e43ff-ab70-0410-9a54-f4433d99c037>
2012-11-05 22:03:26 +0000

    KahinaTreeTest under heavy construction, it alone will not compile.

--- trunk/src/org/kahina/core/test/KahinaTreeTest.java
+++ trunk/src/org/kahina/core/test/KahinaTreeTest.java
@@ -4,23 +4,30 @@ import java.awt.Color;
 import java.io.File;
 import java.io.IOException;
 
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
+import javax.swing.SwingUtilities;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.kahina.core.KahinaInstance;
 import org.kahina.core.control.KahinaEventTypes;
+import org.kahina.core.data.dag.KahinaDAG;
+import org.kahina.core.data.dag.KahinaMemDAG;
 import org.kahina.core.data.tree.KahinaMemTree;
 import org.kahina.core.data.tree.KahinaTree;
 import org.kahina.core.data.tree.LayerDecider;
+import org.kahina.core.visual.dag.KahinaDAGView;
+import org.kahina.core.visual.dag.LayeredLayouter;
 import org.kahina.core.visual.tree.KahinaTreeView;
 import org.kahina.core.visual.tree.KahinaTreeViewMarker;
 import org.kahina.core.visual.tree.KahinaTreeViewOptions;
 import org.kahina.core.visual.tree.KahinaTreeViewPanel;
 import org.kahina.tralesld.TraleSLDInstance;
+import org.kahina.tulipa.TulipaInstance;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
@@ -30,22 +37,65 @@ public class KahinaTreeTest
     {
         try
         {
-        	LayerDecider decider = new TestLayerDecider();
-        	
-            File file = new File("src/org/kahina/core/test/trale-tree.xml");
-            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-            DocumentBuilder db = dbf.newDocumentBuilder();
-            Document dom = db.parse(file);
+            if (args.length == 0)
+            {
+                System.err.println("Usage: java KahinaTreeTest [treeFile]")
+                System.exit(1);
+            }
+            File file = new File(args[0]);
             //TestLayeredTree m1 = TestLayeredTree.importXML(dom);
-            KahinaTree m1 = KahinaMemTree.importXML(dom, decider);
+            KahinaTree dag = KahinaTreeInput.import(dom);  
             
-            file = new File("src/org/kahina/core/test/trale-tree2.xml");
-            dbf = DocumentBuilderFactory.newInstance();
-            db = dbf.newDocumentBuilder();
-            dom = db.parse(file);
-            TestLayeredTree m2 = TestLayeredTree.importXML(dom);
-            //KahinaTree m2 = KahinaDbTree.importXML(dom, decider, data, m1);
+            TulipaInstance kahina = new TulipaInstance();
             
+            final KahinaDAGView v = new KahinaDAGView(kahina, new LayeredLayouter());
+            v.setTitle("Kahina DAGView Demo");
+            v.getConfig().setVerticalDistance(5);
+            v.getConfig().setHorizontalDistance(3);
+            v.display(dag);
+            v.setStatusColorEncoding(0,new Color(255,255,255));
+            v.setStatusColorEncoding(1,new Color(255,0,0));
+            v.setStatusColorEncoding(2,new Color(0,255,255));
+            v.setStatusColorEncoding(3,new Color(255,255,255)); 
+            
+            kahina.registerInstanceListener(KahinaEventTypes.SELECTION, v);
+            kahina.registerInstanceListener(KahinaEventTypes.UPDATE, v);
+            kahina.registerInstanceListener(KahinaEventTypes.REDRAW, v);
+            
+            SwingUtilities.invokeLater(new Runnable() 
+            {
+                public void run() 
+                {
+                    JFrame w = new JFrame("KahinaTAGView Demo");
+                    w.setSize(510, 720);
+                    w.setLayout(new BoxLayout(w.getContentPane(), BoxLayout.LINE_AXIS));
+                    w.add(v.makePanel());
+                    w.setVisible(true);
+                    w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   
+                }
+            });
+        }
+        catch (ParserConfigurationException e)
+        {
+            System.err.println(e.getMessage());
+        }
+        catch (SAXException e)
+        {
+            System.err.println(e.getMessage());
+        }
+        catch (IOException e)
+        {
+            System.err.println(e.getMessage());
+        }
+    }
+        }
+    }
+    public static void main(String[] args)
+    {
+        try
+        {
+        	LayerDecider decider = new TestLayerDecider();
+        	boolean secondaryTree = false;
             KahinaInstance<?, ?, ?, ?> kahina = new TraleSLDInstance();
             
             KahinaTreeView v0 = new KahinaTreeView(kahina);
@@ -54,10 +104,34 @@ public class KahinaTreeTest
             v0.getConfig().setSecondaryLineShapePolicy(KahinaTreeViewOptions.INVISIBLE_LINES);
             v0.getConfig().setVerticalDistance(3);
             v0.getConfig().setHorizontalDistance(18);
-            v0.display(m1,0,17);
-            v0.displaySecondaryTree(m2);
-            v0.getConfig().toggleSecondDimensionDisplay();
-            
+        	File file;
+        	if (args.length > 0)
+        	{
+                file = new File(args[0]);
+                DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+                DocumentBuilder db = dbf.newDocumentBuilder();
+                Document dom = db.parse(file);
+                //TestLayeredTree m1 = TestLayeredTree.importXML(dom);
+                KahinaTree m1 = KahinaMemTree.importXML(dom, decider);
+                v0.display(m1,0,17);    
+        	}
+           
+        	if (args.length != 1)
+        	{
+        	    secondaryTree = true;
+        	    if (args.length == 2)
+        	    {
+        	        File file2 = new File("src/org/kahina/core/test/trale-tree2.xml");
+                    dbf = DocumentBuilderFactory.newInstance();
+                    db = dbf.newDocumentBuilder();
+                    dom = db.parse(file);
+                    TestLayeredTree m2 = TestLayeredTree.importXML(dom);
+                    //KahinaTree m2 = KahinaDbTree.importXML(dom, decider, data, m1);
+                    v0.displaySecondaryTree(m2);
+                    v0.getConfig().toggleSecondDimensionDisplay();
+        	    }
+        	}
+             
             v0.setStatusColorEncoding(0,new Color(0,255,0));
             v0.setStatusColorEncoding(1,new Color(255,0,0));
             v0.setStatusColorEncoding(2,new Color(0,255,255));
