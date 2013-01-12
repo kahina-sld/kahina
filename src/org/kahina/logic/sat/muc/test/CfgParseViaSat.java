cc6ee8f16be563822e115ccea7eff4c8df7ed322
jd <jd@6e8e43ff-ab70-0410-9a54-f4433d99c037>
2013-01-12 15:57:56 +0000

    Updated CfgParseViaSat, started to collect test instances.

--- src/org/kahina/logic/sat/muc/test/CfgParseViaSat.java
+++ src/org/kahina/logic/sat/muc/test/CfgParseViaSat.java
@@ -1,8 +1,10 @@
 package org.kahina.logic.sat.muc.test;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Scanner;
 import java.util.concurrent.TimeoutException;
 
 import org.kahina.logic.sat.data.cnf.CnfSatInstance;
@@ -16,51 +18,75 @@ public class CfgParseViaSat
 {
     public static void main(String[] args)
     {
-        //TODO: sentence file
         if (args.length < 2)
         {
-            System.err.println("Usage: java CfgParseViaSat [CFG file] \"sentence\"");
+            System.err.println("Usage: java CfgParseViaSat [CFG file] [temp directory] ([sentence file])");
             System.exit(0);
         }
-        System.err.println("Loading grammar: " + args[0]);
+        String grammarFile = args[0];
+        String tempDir = args[1];
+        System.err.println("Loading grammar: " + grammarFile);
         ContextFreeGrammar cfg = ContextFreeGrammarParser.parseCFGFile(args[0]);
-        System.err.println("Parsing sentence: " + args[1]);
-        //TODO: optionally generate more interesting symbols (replace ranges by substrings!)
-        CnfSatInstance instance = CfgToSatConverter.parsingToSat(cfg, args[1].split(" "));
-        File tempCnfFile = new File("cfg-parsing-tmp.cnf");
-        File tempResultFile = new File("cfg-parsing-tmp.res");
-        try
+        
+        Scanner in = new Scanner(System.in);
+        if (args.length > 2)
         {
-            tempResultFile.createNewFile();
-            DimacsCnfOutput.writeDimacsCnfFile("cfg-parsing-tmp.cnf", instance);
+            try
+            {
+                in = new Scanner(new File(args[2]));
+            }
+            catch (FileNotFoundException e)
+            {
+                System.err.println("ERROR: file \"" + args[2] + "\" not found!");
+                System.err.println("Entering interactive mode instead.");
+            }
+        }
+        else
+        {
+            System.err.println("Parser is in interactive mode.");
+        }
+        while (in.hasNextLine())
+        {
+            String sentence = in.nextLine();
+            System.err.println("Parsing sentence: " + sentence);
+            String[] words = sentence.split(" ");
+            String filename = sentence.replace(' ', '-');
+            CnfSatInstance instance = CfgToSatConverter.parsingToSat(cfg, words);
+            File tempCnfFile = new File(tempDir + "/" + filename + ".cnf");
+            File tempResultFile = new File(tempDir + "/cfg-parsing-tmp.res");
+            try
+            {
+                tempResultFile.createNewFile();
+                DimacsCnfOutput.writeDimacsCnfFile(tempDir + "/" + filename + ".cnf", instance);
 
-            //TODO: display results symbolically (positive atoms for success, MUS for failure)
-            boolean parseSuccess = MiniSAT.isSatisfiable(tempCnfFile, tempResultFile);
-            if (parseSuccess)
-            {
-                System.err.println("Parse successful!");
+                //TODO: display results symbolically (positive atoms for success, MUS for failure)
+                boolean parseSuccess = MiniSAT.isSatisfiable(tempCnfFile, tempResultFile);
+                if (parseSuccess)
+                {
+                    System.err.println("Parse successful!");
+                }
+                else
+                {
+                    System.err.println("Parse failed!");
+                }
+                //tempCnfFile.delete();
+                tempResultFile.delete();
             }
-            else
+            catch (TimeoutException e)
             {
-                System.err.println("Parse failed!");
+                // TODO Auto-generated catch block
+                e.printStackTrace();
             }
-            tempCnfFile.delete();
-            tempResultFile.delete();
-        }
-        catch (TimeoutException e)
-        {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        catch (InterruptedException e)
-        {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        catch (IOException e)
-        {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+            catch (InterruptedException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            catch (IOException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }      
+        }     
     }
 }
