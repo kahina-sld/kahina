bb1964a6e949b4ead64e45d280840a095b5bda00
jd <jd@6e8e43ff-ab70-0410-9a54-f4433d99c037>
2013-02-27 18:00:31 +0000

    Added Parse example menu to TraleSLD, with sentences defined in test set.

--- trunk/src/org/kahina/tralesld/gui/TraleSLDMainWindow.java
+++ trunk/src/org/kahina/tralesld/gui/TraleSLDMainWindow.java
@@ -1,12 +1,17 @@
 package org.kahina.tralesld.gui;
 
 import org.kahina.core.KahinaInstance;
+import org.kahina.core.control.KahinaActivationEvent;
+import org.kahina.core.control.KahinaActivationStatus;
+import org.kahina.core.data.project.KahinaProjectStatus;
 import org.kahina.core.gui.menus.KahinaProjectMenu;
 import org.kahina.lp.gui.LogicProgrammingMainWindow;
 
 public class TraleSLDMainWindow extends LogicProgrammingMainWindow
 {
 	private static final long serialVersionUID = -8044329699904664157L;
+	
+	TraleSLDParseMenu grammarMenu;
 
 	public TraleSLDMainWindow(TraleSLDWindowManager windowManager, KahinaInstance<?, ?, ?, ?> kahina)
 	{
@@ -20,8 +25,41 @@ public class TraleSLDMainWindow extends LogicProgrammingMainWindow
 	
 	protected void addMenusInFront()
 	{
-		menuBar.add(new TraleSLDParseMenu(kahina));
+	    grammarMenu = new TraleSLDParseMenu(kahina);
+		menuBar.add(grammarMenu);
 		//menuBar.add(new TraleSLDWorkbenchMenu(((TraleSLDGUI) wm.gui)));
 	}
 
+	public void processProjectStatus(KahinaProjectStatus projectStatus)
+    {
+        grammarMenu.setActivationPattern(projectStatus);
+        switch (projectStatus)
+        {
+            case NO_OPEN_PROJECT:
+            {
+                projectMenu.saveProjectItem.setEnabled(false);
+                break;
+            }
+            default:
+            {
+                projectMenu.saveProjectItem.setEnabled(true);
+            }
+        }
+        switch (projectStatus)
+        {
+            case NO_OPEN_PROJECT:
+            {
+                kahina.dispatchInstanceEvent(new KahinaActivationEvent("creep",KahinaActivationStatus.INACTIVE));
+                kahina.dispatchInstanceEvent(new KahinaActivationEvent("skip",KahinaActivationStatus.INACTIVE));
+                kahina.dispatchInstanceEvent(new KahinaActivationEvent("(un)pause",KahinaActivationStatus.INACTIVE));
+                kahina.dispatchInstanceEvent(new KahinaActivationEvent("fail",KahinaActivationStatus.INACTIVE));
+                kahina.dispatchInstanceEvent(new KahinaActivationEvent("leap",KahinaActivationStatus.INACTIVE));
+                kahina.dispatchInstanceEvent(new KahinaActivationEvent("abort",KahinaActivationStatus.INACTIVE));
+                kahina.dispatchInstanceEvent(new KahinaActivationEvent("auto-complete",KahinaActivationStatus.INACTIVE));
+                kahina.dispatchInstanceEvent(new KahinaActivationEvent("backInHistory",KahinaActivationStatus.INACTIVE));
+                kahina.dispatchInstanceEvent(new KahinaActivationEvent("forwardInHistory",KahinaActivationStatus.INACTIVE));
+                break;
+            }
+        }
+    }
 }
