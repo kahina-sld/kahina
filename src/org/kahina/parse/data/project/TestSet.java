c9de477b28b92d34f0f6a36dfb90346aa78f509d
jd <jd@6e8e43ff-ab70-0410-9a54-f4433d99c037>
2013-02-27 16:31:06 +0000

    Added full export functionality for TraleProject.

--- src/org/kahina/parse/data/project/TestSet.java
+++ src/org/kahina/parse/data/project/TestSet.java
@@ -1,8 +1,26 @@
 package org.kahina.parse.data.project;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.kahina.core.data.KahinaObject;
 
 public class TestSet extends KahinaObject
 {
-
+    List<String> sentences;
+    
+    public TestSet()
+    {
+        sentences = new LinkedList<String>();
+    }
+    
+    public List<String> getSentences()
+    {
+        return sentences;
+    }
+    
+    public void addSentence(String sentence)
+    {
+        sentences.add(sentence);
+    }
 }
