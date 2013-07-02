e8ba4ca9f709c77fc0c6c92289590343e1679a1e
jd <jd@6e8e43ff-ab70-0410-9a54-f4433d99c037>
2013-07-02 21:59:49 +0000

    Started Javadoc comments for reduction heuristics interface.

--- trunk/src/org/kahina/logic/sat/muc/heuristics/ReductionHeuristic.java
+++ trunk/src/org/kahina/logic/sat/muc/heuristics/ReductionHeuristic.java
@@ -22,29 +22,56 @@ public abstract class ReductionHeuristic
         this.uc = uc;
     }
     
+    /**
+     * 
+     * @return
+     */
     public boolean needsProof()
     {
         return false;
     }
     
+    /**
+     * Determines whether the heuristic wants to be informed about 
+     * @return
+     */
     public boolean usesProofs()
     {
         return false;
     }
     
+    /**
+     * Is called by the reduction agent to hand on proof trees to the heuristic
+     * for complex calculations, if usesProofs() is defined to return true.
+     * @param proof
+     */
     public void deliverProof(ResolutionProofTree proof)
     {
         
     }
     
+    /**
+     * Does nothing by default
+     * @param criticalClauses
+     */
     public void deliverCriticalClauses(Set<Integer> criticalClauses)
     {
         
     }
     
-    //-1 is the signal that we are ready
+    /**
+     * Is used by the reduction agent steered by this heuristic to poll
+     * reduction candidates. This is the main interface for defining a heuristic,
+     * and it may depend on arbitrarily complex internal calculations.
+     * This method is abstract, so that every heuristic needs to implement it.
+     * @return the next reduction candidate to be tried, -1 to terminate.
+     */
     public abstract int getNextCandidate();
     
+    /**
+     * Defines the name of the heuristic as displayed in the reduction agent system.
+     * @return the name of the heuristic.
+     */
     public String getName()
     {
         return "unkown heuristic";
