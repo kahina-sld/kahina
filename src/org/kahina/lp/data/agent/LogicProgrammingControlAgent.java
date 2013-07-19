eb4252eb8e3993e92f326b4f33edcdd8babde35b
Kilian Evang <texttheater@users.noreply.github.com>
2013-07-19 14:27:21 +0000

    bugfix: unnecessary field (probably a refactoring artifact) was shadowing an important one, causing NullPointerExceptions after the creation of breakpoints (when trying to retrieve the Kahina instance from the control agent)

--- src/org/kahina/lp/data/agent/LogicProgrammingControlAgent.java
+++ src/org/kahina/lp/data/agent/LogicProgrammingControlAgent.java
@@ -1,12 +1,8 @@
 package org.kahina.lp.data.agent;
 
-import org.kahina.core.KahinaInstance;
-import org.kahina.core.control.KahinaController;
 import org.kahina.core.control.KahinaSimpleProperty;
-import org.kahina.core.control.KahinaStepPropertySensor;
 import org.kahina.core.control.KahinaSimplePropertySensor;
 import org.kahina.core.data.agent.KahinaControlAgent;
-import org.kahina.core.data.agent.patterns.TreePatternNode;
 import org.kahina.core.data.tree.KahinaTree;
 import org.kahina.core.io.color.ColorUtil;
 import org.kahina.lp.LogicProgrammingInstance;
@@ -15,19 +11,26 @@ import org.w3c.dom.Element;
 
 public class LogicProgrammingControlAgent extends KahinaControlAgent
 {
+	private static final long serialVersionUID = 915254841399696437L;
+	
+	private static final boolean VERBOSE = false;
     protected KahinaSimplePropertySensor sensor;
-    protected LogicProgrammingInstance<?,?,?,?> kahina;
     
     public LogicProgrammingControlAgent(LogicProgrammingInstance<?,?,?,?> kahina)
     {
         super(kahina);
 
+    	if (VERBOSE)
+    	{
+    		System.err.println(this + ".(" + kahina + ")");
+    	}
         setSensor(new KahinaSimplePropertySensor(this));
     }
     
     public KahinaTree getStepTree()
     {
-        return kahina.getState().getStepTree();
+    	LogicProgrammingState state = kahina.getState();
+        return state.getStepTree();
     }
     
     public KahinaSimplePropertySensor getSensor()
