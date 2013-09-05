af856626807e0960d954f16c71a884eebbdd4999
jd <jd@6e8e43ff-ab70-0410-9a54-f4433d99c037>
2013-09-05 09:50:47 +0000

    Implemented clause block partitioner.

--- src/org/kahina/logic/sat/muc/test/ClauseBlockPartitioner.java
+++ src/org/kahina/logic/sat/muc/test/ClauseBlockPartitioner.java
@@ -1,6 +1,9 @@
 package org.kahina.logic.sat.muc.test;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
 import java.util.TreeSet;
@@ -21,9 +24,16 @@ public class ClauseBlockPartitioner
     
     public static void main(String[] args)
     {
+        if (!MiniSAT.minisatFoundOnPath())
+        {
+            System.err.println("ERROR: No version of minisat found on path!");
+            System.err.println("       Your path must include the minisat-extended directory.");
+            System.exit(0);
+        }
+        
         if (args.length < 1)
         {
-            System.err.println("Usage: ClauseBlockPartitioner [CNF file]");
+            System.err.println("Usage: (< python2.7 marco.py -v [same CNF file] |) java -jar partitionTester.jar [CNF file] ");
             System.exit(1);
         }
         CnfSatInstance instance = DimacsCnfParser.parseDimacsCnfFile(args[0]);
@@ -90,6 +100,7 @@ public class ClauseBlockPartitioner
             reductionID++;
             files.deleteTempFiles();
         }
+        System.out.println("Block structure inferred:");
         for (TreeSet<Integer> block : blockHandler.retrieveBlocks())
         {
             for (int clauseID : block)
@@ -97,6 +108,53 @@ public class ClauseBlockPartitioner
                 System.out.print(clauseID + " ");
             }
             System.out.println();
+        }
+        System.out.println("Reading in clause IDs lists and testing them for representability ... Ctrl+D to quit.");
+        InputStreamReader converter = new InputStreamReader(System.in);
+        BufferedReader in = new BufferedReader(converter);
+        try
+        {
+            String inputLine = in.readLine();
+            int representableCount = 0;
+            while (inputLine != null)
+            {
+                //discard the MSS lines from MARCO's output
+                if (inputLine.startsWith("S")) continue;
+                //take an U at the start as a sign that we are dealing with MARCO's output
+                if (inputLine.startsWith("U"))
+                {
+                    inputLine = inputLine.substring(2);
+                }
+                //just assume we have a space-separated list of clause IDS
+                TreeSet<Integer> testedMUS = new TreeSet<Integer>();
+                for (String idString : inputLine.split(" "))
+                {
+                    testedMUS.add(Integer.parseInt(idString));
+                }
+                if (!blockHandler.checkRepresentability(testedMUS))
+                {
+                    System.err.println("(" + representableCount + ") representable MUSes found");
+                    representableCount = 0;
+                    System.out.print("Unrepresentable MUS found: ");
+                    for (int clauseID : testedMUS)
+                    {
+                        System.out.print(clauseID + " ");
+                    }
+                    System.out.println();
+                }
+                else
+                {
+                    representableCount++;
+                }
+                inputLine = in.readLine();
+            }
+            System.err.println("(" + representableCount + ") representable MUSes found");
+            System.err.println("Finished.");
+        }
+        catch (IOException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
     }
 
